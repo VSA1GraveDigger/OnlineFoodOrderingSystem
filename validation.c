90b6d55fca0a67e4590a4fe09cd8134f2d8f559b
ManasaKarur <manasakarur7@gmail.com>
2021-07-05 01:57:03 +0530

    commit

---
+++ validation.c
@@ -0,0 +1,17 @@
+#include "header.h"
+#include<stdio.h>
+char t_name[100];
+int validation()
+{
+    //name validation
+    int flag;
+    for (int i = 0; t_name[i] != '\0'; i++) {
+        if (!((t_name[i] >= 'a' && t_name[i] <= 'z')
+              || (t_name[i] >= 'A'
+                  && t_name[i] <= 'Z'))) {
+            printf("\nPlease Enter the valid name\n");
+           
+            flag = 0;
+            break;
+        }
+}
