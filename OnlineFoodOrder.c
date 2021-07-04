fd909338ae2369845fd3fdc02514a265d7da3ca5
ManasaKarur <manasakarur7@gmail.com>
2021-07-05 01:42:38 +0530

    commit

--- OnlineFoodOrder.c
+++ OnlineFoodOrder.c
@@ -1,12 +1,13 @@
+#include "header.h" 
 #include<stdio.h>
 #include<stdlib.h>
 int choice;
-char username[50];
-int age;
-char emailid[50];
-char newpassword[50];
-char confirmpassword[50];
-char phno[50];
+//char username[50];
+//int age;
+//char emailid[50];
+//char newpassword[50];
+//char confirmpassword[50];
+//char phno[50];
 
 int main()
 {
@@ -47,24 +48,4 @@ int main()
 
 }
 
-void signup()
-{
-	printf("Enter Your name\t");
-	scanf("%s", username );
 
-	printf("Enter Your Age\t");
-	scanf("%d", &age);
-
-	printf("Enter Your Email\t");
-	scanf("%s", emailid);
-
-	printf("Enter Password\t");
-	scanf("%s", newpassword);
-
-	printf("Confirm Password\t");
-	scanf("%s", confirmpassword);
-
-	printf("Enter Your Mobile Number\t");
-	scanf("%s", phno);
-
-}
---
+++ signup.c
@@ -0,0 +1,23 @@
+#include "header.h"
+#include<stdio.h>
+void signup()
+{
+	printf("Enter Your name\t");
+	scanf("%s", username );
+
+	printf("Enter Your Age\t");
+	scanf("%d", &age);
+
+	printf("Enter Your Email\t");
+	scanf("%s", emailid);
+
+	printf("Enter Password\t");
+	scanf("%s", newpassword);
+
+	printf("Confirm Password\t");
+	scanf("%s", confirmpassword);
+
+	printf("Enter Your Mobile Number\t");
+	scanf("%s", phno);
+
+}
