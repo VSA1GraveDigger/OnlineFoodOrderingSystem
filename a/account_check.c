771ad116d2b6bc54ffbfbc06d40c35c73e867e6c
ManasaKarur <manasakarur7@gmail.com>
2021-07-06 19:18:25 +0530

    commit

---
+++ a/account_check.c
@@ -0,0 +1,43 @@
+// Function to check if the account already exists or not
+#include "header.h"
+#include "OnlineFoodOrder.c"
+#include<stdio.h>
+char t_email[50],t_name[100];
+char t_password1[100];
+int t_mobile[50];
+int iter,j;
+int t_age[50];
+
+void acc_check()
+{
+    
+    for (iter = 0; iter < 100; iter++) {
+        // Check whether the email and password are already matched with existed account
+        if (!(strcmp(t_email,
+                     s[iter].email)
+              && strcmp(t_password1,
+                        s[iter].password))) {
+            printf("\n\nAccount Already");
+            printf("Existed. Please"
+                   " Login !!\n\n");
+            main();
+            break;
+        }
+    }
+  
+    // If account does not already
+    // existed, it creates a new
+    // one with new inputs
+    if (iter == 100) {
+        strcpy(s[j].uname, t_name);
+        s[j].age = t_age;
+        strcpy(s[j].password,
+               t_password1);
+        strcpy(s[j].email, t_email);
+        strcpy(s[j].mobile, t_mobile);
+        j++;
+        printf("\n\n\nAccount"
+               " Successfully");
+        printf("Created!!\n\n");
+    }
+}
