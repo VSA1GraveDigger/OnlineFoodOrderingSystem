c11a5ab8eb9e44f5d7048c2fee7269c9dd50178b
ManasaKarur <manasakarur7@gmail.com>
2021-07-06 22:05:12 +0530

    commit

--- header.h
+++ header.h
@@ -3,6 +3,8 @@ void login();
 int validation();
 int main();
 void account_check();
+void search_by_hotels();
+// Structure to store the details
 typedef struct details {
     char uname[100];
     int age;
@@ -11,4 +13,16 @@ typedef struct details {
     char mobile[10];
 }detail;
 detail s[100];
+// Structure to store the hotels
+
+struct hotels {
+    char hotel[100];
+    char first_food[20];
+    char second_food[20];
+    char third_food[20];
+    char fourth_food[25];
+    int first, second, third, fourth;
+};
+
+struct hotels m[5];
 
