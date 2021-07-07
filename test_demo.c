6eb8f387acb5c37911ad3387d79f1ca2c485f9cb
ManasaKarur <manasakarur7@gmail.com>
2021-07-07 13:41:04 +0530

    commit

--- test_demo.c
+++ test_demo.c
@@ -13,6 +13,8 @@ void tearDown()
 void test_miniproject(void)
 {
     char t_newpassword[50],t_confirmpassword[50];
+    scanf("%s",t_newpassword);
+    scanf("%s",t_confirmpassword);
     TEST_ASSERT_EQUAL_STRING_MESSAGE(t_newpassword ,t_confirmpassword  , "password matches");
     TEST_ASSERT_EQUAL_STRING_MESSAGE(t_newpassword , t_confirmpassword , "password matches");
     
